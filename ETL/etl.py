e0710a409823c42f6e6f7da3de102671f98aa15e
Avalon <git@avalon.wtf>
2024-03-09 21:52:43 -0800

    make a thing that works

--- ETL/etl.py
+++ ETL/etl.py
@@ -1,34 +1,30 @@
 ### HERE BE DRAGONS
 from pathlib import Path
 import os
-import pyspark
-from pyspark.sql import SparkSession, DataFrameReader
-from pyspark.sql.functions import col, min, max
-from pyspark.sql.types import *
-from pyspark.rdd import *
-import pandas as pd
+#########################
+# Not necessary for now #
+#########################
+#import pyspark
+#from pyspark.sql import SparkSession, DataFrameReader
+#from pyspark.sql.functions import col, min, max
+#from pyspark.sql.types import *
+#from pyspark.rdd import *
+#import pandas as pd
 import json
-import ijson
-import sqlalchemy
+# import sqlalchemy
 import git
-#???
-os.environ["HADOOP_HOME"] = 'C:\\hadoop'
+
 # sample mcbroken.json file
 FILE_PATH = Path.cwd() / 'ETL' / 'mcbroken.json'
 
 # extract mcbroken.json archives
-MCBROKEN_ARCHIVE_REPO_PATH = Path('../mcbroken-archive')
-os.chdir(MCBROKEN_ARCHIVE_REPO_PATH)
-MCBROKEN_ARCHIVE_REPO_PATH = Path.cwd()
 
-spark_mb = SparkSession \
-    .builder \
-    .appName('mcbroken-archive data') \
-    .getOrCreate()
-pyspark.SparkContext.setSystemProperty('hadoop.home.dir', 'C:\\hadoop\\bin')
+# this will all change once we have a real place to retrieve and store data from
+mcbroken_archive_repo_path = Path(os.environ.get('HOME')) / 'code' / 'mcbroken-archive'
+os.chdir(mcbroken_archive_repo_path)
 
-def jack_the_ripper(repo_path: Path = Path.cwd(),
-                    dump_dir:  Path = Path('./DATA_DUMP')
+def do_the_thing(repo_path: Path = Path.cwd(),
+                    dump_dir:  Path = None
                     ) -> list:
     """Extracts and transforms archived datasets from a git repo,
     sequencing them in a directory from oldest to newest.
@@ -43,6 +39,8 @@ def jack_the_ripper(repo_path: Path = Path.cwd(),
     # Ensure we're in the right place.
     os.chdir(repo_path) 
     # If does not exist, create target dir
+    if dump_dir == None:
+        dump_dir = repo_path / 'DATA_DUMP'
     dump_dir.mkdir(parents=True, exist_ok=True)
     generate_dump_data(repo_path, dump_dir)
     #extract_mcbroken_archive(repo_path, dump_dir)
@@ -52,7 +50,7 @@ def jack_the_ripper(repo_path: Path = Path.cwd(),
                 
 
 def generate_dump_data(repo_path: Path=Path.cwd(),
-                      dump_dir: Path=(Path.cwd() / 'DATA_DUMP')
+                      dump_dir: Path=None
                       ) -> int:
     """Stores archive dump to dataframe
 
@@ -64,122 +62,100 @@ def generate_dump_data(repo_path: Path=Path.cwd(),
         None
     """
     repo = git.Repo(repo_path)
-    data_dump = dump_dir / 'dump.pkl'
-    #check if file can be loaded
-    if data_dump.is_file():
-        df = spark_mb.SparkContext.pickleFile('DATA_DUMP/dump.pkl')
-    #otherwise, create empty:
-    else:
-        empty_RDD = spark_mb.sparkContext.emptyRDD()
-        df_schema = StructType([
-            StructField('commit_hash', StringType(), True),
-            StructField('extracted', StringType(), True),
-            StructField('transformed', StringType(), True),
-            StructField('loaded', StringType(), True),
-            StructField('git_commit_time', StringType(), True),
-            StructField('contents', StringType(), True)
-        ])
-        df = spark_mb.createDataFrame(data = empty_RDD,
-                                schema = df_schema)
+    repo.remotes.origin.fetch()
     commit_list = repo.git.log('--pretty=%h', '--all', '--', './mcbroken.json')
     #convert string output to list
     commit_list = commit_list.split('\n')
-    df = df.toPandas()
-    for each in commit_list[0:5]:
-        #if df.lookup(each):
-        #    continue
-        #else:
-            print(f'Processing \'{each}\'...')
-            current_commit_time = repo.git.show('--no-patch', '--pretty=format:%ct', each)
+    commit_times_json_path = dump_dir / '000_COMMIT_TIMES.json'
+    for each in commit_list:
+        current_path = dump_dir / f'{each}.json'
+        if current_path.exists():
+            continue
+        else:
+            print(f'Fetching data for \'{each}\'...')
             current_contents = repo.git.show(f'{each}:mcbroken.json')
-            df._append([[each, 'True', 'False', 'False', current_commit_time, current_contents]])
-    df = spark_mb.createDataFrame(data = df,
-                                  schema = df_schema)
-    df.rdd.saveAsPickleFile('DATA_DUMP/dump.pkl')
+            with open(current_path, 'w+') as f:
+                f.write(current_contents)
+            if commit_times_json_path.is_file() != True:
+                with open(commit_times_json_path, 'w+') as f:
+                    commit_times = dict()
+                    commit_times[each] = repo.git.show('--no-patch', '--pretty=format:%ct', each)
+                    commit_times_as_json = json.dumps(commit_times)
+                    f.write(commit_times_as_json)
+            else:
+                with open(commit_times_json_path, 'r+') as f:
+                    commit_times = json.load(f)
+                    commit_times[each] = repo.git.show('--no-patch', '--pretty=format:%ct', each)
+                    f.seek(0)
+                    commit_times_as_json = json.dumps(commit_times)
+                    print(len(commit_times))
+                    f.write(commit_times_as_json)
+
+            
 
 
-def generate_dump_json(repo_path: Path=Path.cwd(),
-                      dump_dir: Path=(Path.cwd() / 'DATA_DUMP')
-                      ) -> int:
-    """Generates log of archive dump as a json file.
 
-    Args:
-        repo_path (Path, optional): Path for archive repo. Defaults to Path.cwd().
-        dump_dir (Path, optional): Path for dump directory. Defaults to (Path.cwd() / 'DATA_DUMP').
 
-    Returns:
-        None
-    """
-    # Saving this for later
-    mcbrokenjson_path = repo_path / 'mcbroken.json'
-    # Creates a logfile if it doesn't exist
-    dump_json_path = dump_dir / 'dump.json'
-    if dump_json_path.is_file() != True:
-        with open(dump_json_path, 'w') as f:
-            empty_dict = {}
-            f.write(json.dump(empty_dict, 'w+'))
-            pass
-    repo = git.Repo(repo_path)
-    # list every commit in which mcbroken.json has been
-    # modified in reverse chronological order
-    #rev_list = repo.git.log('--all', '--objects', '--', './mcbroken.json')
-    commit_list = repo.git.log('--pretty=%h', '--all', '--', './mcbroken.json')
-    #convert string output to list
-    commit_list = commit_list.split('\n')
-    with open(dump_dir / 'dump.json', 'r+') as dumper:
-        #check if file already has data
-        try:
-            data = json.load(dumper)
-        except ValueError:
-            print(f'File {dump_json_path} is empty. Generating data - this may take a while!')
-            data = dict()
-        print('Checking and updating dump.json')
-        for each in commit_list:
-            if each in data:
-               pass
-            else:
-                data[each] = {
-                    'properties': {},
-                    'contents': {}
-                    }
-                print(f'Adding entry for \'{each}\'...')
-            if 'properties' in data[each]:
-                pass
-            else:
-                raise ValueError(f'\'{each}\' says, "Properties? In this economy?"')
-            if 'contents' in data[each]:
-                pass
-            else:
-                raise ValueError(f'\'{each}\' wants to know if this data is ethically sourced!')
-            if 'git_commit_time' in data[each]['properties']:
-                pass
-            else:
-                data[each]['properties']['git_commit_time'] = repo.git.show('--no-patch', '--pretty=format:%ct', each)
-            if 'extracted_flag' in data[each]['properties']:
-                pass
-            else:
-                data[each]['properties']['extracted_flag'] = 'False'
-            if 'transformed_flag' in data[each]['properties']:
-                pass
-            else:
-                data[each]['properties']['transformed_flag'] = 'False'
-        data = json.dumps(data, sort_keys=True, indent=4)
-        dumper.seek(0)
-        dumper.truncate()
-        dumper.write(data)
-    print(f'Finished processing \'{dump_json_path}\'.')
-    return None
+#def generate_dump_data(repo_path: Path=Path.cwd(),
+#                      dump_dir: Path=None
+#                      ) -> int:
+#    """Stores archive dump to dataframe
+#
+#    Args:
+#        repo_path (Path, optional): Path for archive repo. Defaults to Path.cwd().
+#        dump_dir (Path, optional): Path for dump directory. Defaults to (Path.cwd() / 'DATA_DUMP').
+#
+#    Returns:
+#        None
+#    """
+#    repo = git.Repo(repo_path)
+#    if dump_dir == None:
+#        dump_dir = repo_path / 'DATA_DUMP'
+#    dump_parquet = dump_dir / 'dump.parquet'
+#    df_schema = StructType([
+#            StructField('commit_hash', StringType(), True),
+#            StructField('extracted', StringType(), True),
+#            StructField('transformed', StringType(), True),
+#            StructField('loaded', StringType(), True),
+#            StructField('git_commit_time', StringType(), True),
+#            StructField('contents', StringType(), True)
+#        ])
+#    #check if file can be loaded
+#    if dump_parquet.is_file():
+#        df = spark_mb.read.load(str(dump_parquet))
+#    #otherwise, create empty:
+#    else:
+#        empty_RDD = spark_mb.sparkContext.emptyRDD()
+#        df = spark_mb.createDataFrame(data = empty_RDD,
+#                                schema = df_schema)
+#    commit_list = repo.git.log('--pretty=%h', '--all', '--', './mcbroken.json')
+#    #convert string output to list
+#    commit_list = commit_list.split('\n')
+#    df_temp = df.toPandas()
+#    df_temp.set_index('commit_hash')
+#    for each in commit_list:
+#        if each in df_temp.index:
+#            continue
+#        else:
+#            print(f'Processing \'{each}\'...')
+#            current_commit_time = repo.git.show('--no-patch', '--pretty=format:%ct', each)
+#            current_contents = repo.git.show(f'{each}:mcbroken.json')
+#            df_temp._append([[each, 'True', 'False', 'False', current_commit_time, current_contents]])
+#    df = spark_mb.createDataFrame(data = df_temp,
+#                                  schema = df_schema)
+#    print(f'Done processing! Writing to \'{str(dump_parquet)}...\'')
+#    df.write.mode('overwrite').format('parquet').save(str(dump_parquet))
 
-def extract_mcbroken_archive(repo_path: Path=Path.cwd(),
-                             data_dump: Path=(Path.cwd() / 'DATA_DUMP')
-                             ) -> None:
-    repo = git.Repo(repo_path)
-    dump_json = data_dump / 'dump.json'
-    with open(dump_json, 'r+') as f:
+#def extract_mcbroken_archive(repo_path: Path=Path.cwd(),
+#                             data_dump: Path=(Path.cwd() / 'DATA_DUMP')
+#                             ) -> None:
+#    repo = git.Repo(repo_path)
+#    dump_json = data_dump / 'dump.json'
+#    with open(dump_json, 'r+') as f:
         # TODO: use pd instead of json to see if that fixes memory leak
         # TODO: possibly flatten json a bit
-        df = spark.read.json('DATA_DUMP/dump.json')
-        df.show()
+#        df = spark_mb.read
+#        df.show()
         #temp_file = data_dump / 'TEMP.json'
         #k = data.keys()
         #for each in iter(k):
@@ -201,7 +177,7 @@ def extract_mcbroken_archive(repo_path: Path=Path.cwd(),
             #    temp_data[each] = data[each]
             #    temp_data[each]['contents'] = repo.git.show(f'{each}:mcbroken.json')
             #    f.write(json.dumps(temp_data))
-        return None
+#        return None
                 
 
     
@@ -234,4 +210,4 @@ def extract_mcbroken_archive(repo_path: Path=Path.cwd(),
 
 ### Scratchpad
 if __name__ == '__main__':
-    set_cpu_ablaze = jack_the_ripper(MCBROKEN_ARCHIVE_REPO_PATH)
+    computer_on_fire = do_the_thing(mcbroken_archive_repo_path)
