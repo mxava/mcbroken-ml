6a686054bd6e646e40e14103ef94334ee1b78601
Avalon <git@avalon.wtf>
2024-03-24 23:19:55 -0700

    extract and fix_times methods

--- ETL/etl.py
+++ ETL/etl.py
@@ -10,22 +10,17 @@ import os
 #from pyspark.sql.types import *
 #from pyspark.rdd import *
 #import pandas as pd
+import requests
 import json
 # import sqlalchemy
 import git
-
-
-# extract mcbroken.json archives
-
-# this will all change once we have a real place to retrieve and store data from
-mcbroken_archive_repo_path = Path(os.environ.get('HOME')) / 'code' / 'mcbroken-archive'
-os.chdir(mcbroken_archive_repo_path)
 
 
 class DataAggregate:
     
     def __init__(self, name):
         self.name = name
+        self.df # empty dataframe
 
     ### Save / Load Methods
 
@@ -46,24 +41,22 @@ class DataAggregate:
 
 class McBrokenData:
     
-    def __init__(self, name:str, repo_url:str, working_dir: Path):
+    def __init__(self, repo_url:str = 'https://github.com/rashiq/mcbroken-archive.git', name:str = None, repo_dir: Path = None, dump_dir: Path = None):
         self.name = name
         self.source_url = repo_url
-        self.working_dir = self.set_working_dir()
+        self.repo_dir = repo_dir if repo_dir else (Path.cwd() / 'mcbroken-archive')
+        self.dump_dir = dump_dir if dump_dir else (self.repo_dir / 'DATA_DUMP')
         git_repo_init = self.initialize_git_repo()
-        self.repo = self.initialize_git_repo[0]
+        self.repo = git_repo_init[0]
         self.is_initialized = git_repo_init[1]
-
-    def set_working_dir(working_dir) -> Path:
-        return working_dir if working_dir is not None else (Path.cwd() / 'mcbroken-archive')
     
     def initialize_git_repo(
-            mcbroken_archive_url_git:str='https://github.com/rashiq/mcbroken-archive.git',
-            branch:str='main',
+            self,
+            mcbroken_archive_url_git:str = None,
             working_tree_dir = None
-            ) -> Path:
+            ) -> tuple:
+        source_url = mcbroken_archive_url_git if mcbroken_archive_url_git is not None else self.source_url
         repo_dir = working_tree_dir if working_tree_dir is not None else (Path.cwd() / 'mcbroken-archive')
-        
         # Attempt to initialize repo
         try:
             repo = git.Repo(repo_dir)
@@ -71,9 +64,9 @@ class McBrokenData:
             assert repo.git_dir
         except:
             try:
-                repo = git.Repo.clone_from(mcbroken_archive_url_git,
-                           repo_dir,
-                           branch)
+                repo = git.Repo.clone_from(source_url,
+                           str(repo_dir),
+                           branch='main')
                 assert repo.git_dir
             except:
                 is_initialized = False
@@ -82,11 +75,69 @@ class McBrokenData:
         is_initialized = True
         return repo, is_initialized
     
-    def extract(working_dir = None):
-        working_dir = working_dir if working_dir is not None else self.working_dir
+    def extract(self):
         commit_list = self.repo.git.log('--pretty=%h', '--all', '--', './mcbroken.json')
-        print(commit_list)
         # Generate dump data
+        repo = self.repo
+        repo.remotes.origin.fetch()
+        if not self.dump_dir.exists():
+            self.dump_dir.mkdir(parents=True, exist_ok=True)
+        commit_list = repo.git.log('--pretty=%h', '--all', '--', './mcbroken.json')
+        #convert string output to list
+        commit_list = commit_list.split('\n')
+        commit_times_json_path = self.dump_dir / '000_COMMIT_TIMES.json'
+        for each in commit_list:
+            current_file_path = self.dump_dir / f'{each}.json'
+            try:
+                assert current_file_path.exists()
+                continue
+            except:
+                print(f'Fetching data for \'{each}\'...')
+                current_contents = repo.git.show(f'{each}:mcbroken.json')
+                with open(current_file_path, 'w+') as f:
+                    f.write(current_contents)
+                if commit_times_json_path.is_file() != True:
+                    with open(commit_times_json_path, 'w+') as f:
+                        commit_times = dict()
+                        commit_times[each] = {}
+                        commit_times[each]['commit_time'] = repo.git.show('--no-patch', '--pretty=format:%ct', each)
+                        commit_times[each]['time_fixed'] = False
+                        commit_times_as_json = json.dumps(commit_times, sort_keys=True, indent=4)
+                        f.write(commit_times_as_json)
+                else:
+                    with open(commit_times_json_path, 'r+') as f:
+                        commit_times = json.load(f)
+                        commit_times[each] = {}
+                        commit_times[each]['commit_time'] = repo.git.show('--no-patch', '--pretty=format:%ct', each)
+                        commit_times[each]['time_fixed'] = False
+                        commit_times[each]['processed'] = False
+                        f.seek(0)
+                        commit_times_as_json = json.dumps(commit_times, sort_keys=True, indent=4)
+                        f.write(commit_times_as_json)
+
+    def fix_times(self):
+        commit_times_json_path = self.dump_dir / '000_COMMIT_TIMES.json'
+        with open(commit_times_json_path, 'r+') as commit_times:
+            commit_times_dict = json.load(commit_times)
+            for each_commit in list(commit_times_dict.keys()):
+                if commit_times_dict[each_commit]['time_fixed'] is not True:
+                    print(f'Adjusting \'last_checked\' values for {each_commit}.json...')
+                    current_mcbroken_json = self.dump_dir / each_commit+'.json'
+                    with open(current_mcbroken_json, 'r+') as f:
+                        current_mcbroken_contents = json.load(f)
+                        for each_entry in current_mcbroken_contents:
+                            value_to_subtract_in_seconds = int(each_entry['properties']['last_checked'].split(' ')[1]) * 60
+                            each_entry['properties']['last_checked'] = commit_times_dict[each_commit]['commit_time'] - value_to_subtract_in_seconds
+                        f.seek(0)
+                        new_mcbroken_contents = json.dumps(current_mcbroken_contents, sort_keys=True, indent=4)
+                        f.write(new_mcbroken_contents)
+                    commit_times_dict[each_commit]['time_fixed'] = True
+                    continue
+            commit_times.seek(0)
+            commit_times_as_json = json.dumps(commit_times_dict)
+            commit_times.write(commit_times_as_json)
+
+
 
 
         
@@ -96,75 +147,8 @@ class WeatherData:
 
     # TODO: Think about how to structure McBroken db,
     # followed by how best to fetch, parse, and load weather data
-
-
-# TODO: Deprecate the below functions once converted to a working method
-def do_the_thing(repo_path: Path = Path.cwd(),
-                    dump_dir:  Path = None
-                    ) -> list:
-    """Extracts and transforms archived datasets from a git repo,
-    sequencing them in a directory from oldest to newest.
-
-    Args:
-        repo (Path): git repo path
-        dump_dir (str): target directory to dump dataset
-
-    Returns:
-        output : itemized list of files added
-    """
-    # Ensure we're in the right place.
-    os.chdir(repo_path) 
-    # If does not exist, create target dir
-    if dump_dir == None:
-        dump_dir = repo_path / 'DATA_DUMP'
-    dump_dir.mkdir(parents=True, exist_ok=True)
-    generate_dump_data(repo_path, dump_dir)
-    # TODO: Add the next thing
-    return
-                
-
-
-def generate_dump_data(repo_path: Path=Path.cwd(),
-                      dump_dir: Path=None
-                      ) -> int:
-    """Stores archive dump to dataframe
-
-    Args:
-        repo_path (Path, optional): Path for archive repo. Defaults to Path.cwd().
-        dump_dir (Path, optional): Path for dump directory. Defaults to (Path.cwd() / 'DATA_DUMP').
-
-    Returns:
-        None
-    """
-    repo = git.Repo(repo_path)
-    repo.remotes.origin.fetch()
-    commit_list = repo.git.log('--pretty=%h', '--all', '--', './mcbroken.json')
-    #convert string output to list
-    commit_list = commit_list.split('\n')
-    commit_times_json_path = dump_dir / '000_COMMIT_TIMES.json'
-    for each in commit_list:
-        current_path = dump_dir / f'{each}.json'
-        if current_path.exists():
-            continue
-        else:
-            print(f'Fetching data for \'{each}\'...')
-            current_contents = repo.git.show(f'{each}:mcbroken.json')
-            with open(current_path, 'w+') as f:
-                f.write(current_contents)
-            if commit_times_json_path.is_file() != True:
-                with open(commit_times_json_path, 'w+') as f:
-                    commit_times = dict()
-                    commit_times[each] = repo.git.show('--no-patch', '--pretty=format:%ct', each)
-                    commit_times_as_json = json.dumps(commit_times)
-                    f.write(commit_times_as_json)
-            else:
-                with open(commit_times_json_path, 'r+') as f:
-                    commit_times = json.load(f)
-                    commit_times[each] = repo.git.show('--no-patch', '--pretty=format:%ct', each)
-                    f.seek(0)
-                    commit_times_as_json = json.dumps(commit_times)
-                    print(len(commit_times))
-                    f.write(commit_times_as_json)
+    def __init__(self):
+        return
 
 
 # TODO: Figure out SQL DB schema/desired data
@@ -184,4 +168,6 @@ def generate_dump_data(repo_path: Path=Path.cwd(),
 
 ### Scratchpad
 if __name__ == '__main__':
-    mcbroken_data = McBrokenData
+    mcbroken_data = McBrokenData(name='mcbroken')
+    mcbroken_data.extract()
+    mcbroken_data.fix_times()
